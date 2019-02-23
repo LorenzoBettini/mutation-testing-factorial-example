638acb22fba27bd13cf01b20597b65c3884b06c7
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-23 15:12:50 +0100

    initial

---
+++ mutation.testing.factorial.example/tests/factorial/example/FactorialTest.java
@@ -0,0 +1,34 @@
+package factorial.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FactorialTest {
+
+	private Factorial factorial;
+
+	@Before
+	public void setup() {
+		factorial = new Factorial();
+	}
+
+	@Test
+	public void testBaseCase() {
+		assertEquals(1, factorial.compute(0));
+	}
+
+	@Test
+	public void testInductiveCase() {
+		assertEquals(5 * factorial.compute(4), factorial.compute(5));
+	}
+
+	@Test
+	public void testNegativeInput() {
+		IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
+			() -> factorial.compute(-1));
+		assertEquals("Negative input: -1", thrown.getMessage());
+	}
+}
