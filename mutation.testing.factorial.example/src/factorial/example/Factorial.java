30eedfc8f23c43782a673a8da5766ab74695d937
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-03-09 18:53:27 +0100

    remove useless if

--- mutation.testing.factorial.example/src/factorial/example/Factorial.java
+++ mutation.testing.factorial.example/src/factorial/example/Factorial.java
@@ -5,8 +5,6 @@ public class Factorial {
 	public int compute(int n) {
 		if (n < 0)
 			throw new IllegalArgumentException("Negative input: " + n);
-		if (n == 0)
-			return 1;
 		int result = 1;
 		while (n >= 2) {
 			result *= n--;
