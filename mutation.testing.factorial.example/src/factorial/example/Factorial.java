a014ef86bfe2e165e620fc704c843c698bbad299
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-03-09 19:02:02 +0100

    iterative version with streams

--- mutation.testing.factorial.example/src/factorial/example/Factorial.java
+++ mutation.testing.factorial.example/src/factorial/example/Factorial.java
@@ -1,15 +1,13 @@
 package factorial.example;
 
+import java.util.stream.IntStream;
+
 public class Factorial {
 
 	public int compute(int n) {
 		if (n < 0)
 			throw new IllegalArgumentException("Negative input: " + n);
-		int result = 1;
-		while (n >= 2) {
-			result *= n--;
-		}
-		return result;
+		return IntStream.rangeClosed(2, n).reduce(1, (x, y) -> x * y);
 	}
 
 }
